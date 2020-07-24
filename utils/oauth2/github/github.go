bb8200f6b04a4b91c356f17b249449ad513418a9
davve <davve@qq.com>
2020-07-24 22:54:00 +0800

    拆分

---
+++ utils/oauth2/github/github.go
@@ -0,0 +1,152 @@
+package github
+
+import (
+	"encoding/json"
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+type Conf struct {
+	ClientId     string // 对应: Client ID
+	ClientSecret string // 对应: Client Secret
+	RedirectUrl  string // 对应: Authorization callback URL
+}
+
+var conf = Conf{
+	ClientId:     "7e5fe351bc9b131c6f2a",
+	ClientSecret: "9fd22c13ae790685c59e3fb4a9b444b75b506a5b",
+	RedirectUrl:  "http://localhost:9090/oauth/redirect",
+}
+
+type Token struct {
+	AccessToken string `json:"access_token"`
+	TokenType   string `json:"token_type"` // 这个字段下面没用到
+	Scope       string `json:"scope"`      // 这个字段下面也没用到
+}
+
+// 返回欢迎页面
+func Hello(w http.ResponseWriter, r *http.Request) {
+	// 解析指定文件生成模板对象
+	var temp *template.Template
+	var err error
+	if temp, err = template.ParseFiles("views/hello.html"); err != nil {
+		fmt.Println("读取文件失败，错误信息为:", err)
+		return
+	}
+
+	// 利用给定数据渲染模板(html页面)，并将结果写入w，返回给前端
+	if err = temp.Execute(w, conf); err != nil {
+		fmt.Println("读取渲染html页面失败，错误信息为:", err)
+		return
+	}
+}
+
+// 认证并获取用户信息
+func Oauth(w http.ResponseWriter, r *http.Request) {
+
+	var err error
+
+	// 获取 code
+	var code = r.URL.Query().Get("code")
+
+	// 通过 code, 获取 token
+	var tokenAuthUrl = GetTokenAuthUrl(code)
+	var token *Token
+	if token, err = GetToken(tokenAuthUrl); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// 通过token，获取用户信息
+	var userInfo map[string]interface{}
+	if userInfo, err = GetUserInfo(token); err != nil {
+		fmt.Println("获取用户信息失败，错误信息为:", err)
+		return
+	}
+
+	//  将用户信息返回前端
+	var userInfoBytes []byte
+	if userInfoBytes, err = json.Marshal(userInfo); err != nil {
+		fmt.Println("在将用户信息(map)转为用户信息([]byte)时发生错误，错误信息为:", err)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	if _, err = w.Write(userInfoBytes); err != nil {
+		fmt.Println("在将用户信息([]byte)返回前端时发生错误，错误信息为:", err)
+		return
+	}
+
+}
+
+// 通过code获取token认证url
+func GetTokenAuthUrl(code string) string {
+	return fmt.Sprintf(
+		"https://github.com/login/oauth/access_token?client_id=%s&client_secret=%s&code=%s",
+		conf.ClientId, conf.ClientSecret, code,
+	)
+}
+
+// 获取 token
+func GetToken(url string) (*Token, error) {
+
+	// 形成请求
+	var req *http.Request
+	var err error
+	if req, err = http.NewRequest(http.MethodGet, url, nil); err != nil {
+		return nil, err
+	}
+	req.Header.Set("accept", "application/json")
+
+	// 发送请求并获得响应
+	var httpClient = http.Client{}
+	var res *http.Response
+	if res, err = httpClient.Do(req); err != nil {
+		return nil, err
+	}
+
+	// 将响应体解析为 token，并返回
+	var token Token
+	if err = json.NewDecoder(res.Body).Decode(&token); err != nil {
+		return nil, err
+	}
+	return &token, nil
+}
+
+// 获取用户信息
+func GetUserInfo(token *Token) (map[string]interface{}, error) {
+
+	// 形成请求
+	var userInfoUrl = "https://api.github.com/user" // github用户信息获取接口
+	var req *http.Request
+	var err error
+	if req, err = http.NewRequest(http.MethodGet, userInfoUrl, nil); err != nil {
+		return nil, err
+	}
+	req.Header.Set("accept", "application/json")
+	req.Header.Set("Authorization", fmt.Sprintf("token %s", token.AccessToken))
+
+	// 发送请求并获取响应
+	var client = http.Client{}
+	var res *http.Response
+	if res, err = client.Do(req); err != nil {
+		return nil, err
+	}
+
+	// 将响应的数据写入 userInfo 中，并返回
+	var userInfo = make(map[string]interface{})
+	if err = json.NewDecoder(res.Body).Decode(&userInfo); err != nil {
+		return nil, err
+	}
+	return userInfo, nil
+}
+
+func main() {
+	http.HandleFunc("/", Hello)
+	http.HandleFunc("/oauth/redirect", Oauth) // 这个和 Authorization callback URL 有关
+
+	if err := http.ListenAndServe(":9090", nil); err != nil {
+		fmt.Println("监听失败，错误信息为:", err)
+		return
+	}
+}
