0ad77a5f999fd154d9b04b016490597569423dc7
davve <921971751@qq.com>
2020-07-26 22:12:35 +0800

    wechat

--- utils/oauth2/wechat/wechat.go
+++ utils/oauth2/wechat/wechat.go
@@ -1,6 +1,8 @@
 package wechat
 
 // https://blog.csdn.net/weixin_43851310/article/details/105816815
+// https://github.com/yun-mu/weixin-login/blob/master/src/controller/weixin.go
+// https://github.com/yizenghui/WechatQrcodeServe
 // TODO 微信授权登录
 
 func GenRedirectURL() string {
