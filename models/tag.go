687f78f0cdcc18ffda0bfd08cd4cd4624be5857f
davve <davve@qq.com>
2020-08-03 18:23:20 +0800

    tag model

--- models/tag.go
+++ models/tag.go
@@ -1,1 +1,12 @@
 package models
+
+type Tag struct {
+	BaseModel
+	ZhName string `json:"zh_name" gorm:"column:zh_name;not null;unique;comment:'标签中文名'"`
+	EnName string `json:"en_name" gorm:"column:en_name;not null;unique;comment:'标签英文名'"`
+	IsHot  int    `json:"-"  gorm:"default:0;comment:'是否为热门标签 0:不是, 1:是'"`
+}
+
+func (Tag) TableName() string {
+	return "tb_tag"
+}
