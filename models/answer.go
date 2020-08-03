38fa2764556dca6d38696b3412fdc833f854386c
davve <davve@qq.com>
2020-08-03 18:26:40 +0800

    问题与回答

--- models/answer.go
+++ models/answer.go
@@ -1,1 +1,12 @@
 package models
+
+type Answer struct {
+	BaseModel
+	QuestionId string
+	UserId     string
+	Content    string
+}
+
+func (Answer) TableName() string {
+	return "tb_answer"
+}
--- models/question.go
+++ models/question.go
@@ -1,1 +1,12 @@
 package models
+
+type Question struct {
+	UserId  string
+	Title   string
+	Desc    string
+	Content string
+}
+
+func (Question) TableName() string {
+	return "tb_question"
+}
